4a2172178a1bc070671bf694320e67b33af57f02
AyushNegixd <ayushnegi200051@gmail.com>
2026-01-05 12:29:36 +0530

    Day 18

--- js-solutions/day-18-Debounce.js
+++ js-solutions/day-18-Debounce.js
@@ -7,3 +7,14 @@
 // The first 2 function calls would be cancelled, and the 3rd function call would be executed at 150ms.
 
 // If instead t = 35ms, The 1st call would be cancelled, the 2nd would be executed at 95ms, and the 3rd would be executed at 135ms.
+
+
+var debounce = function(fn, t) {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn(...args)
+        }, t);
+    }
+};
